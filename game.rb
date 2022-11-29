ec7fdd1626a7499d74cb1867f8c6bee899d99b01
Vlad <m.vlad.popa@gmail.com>
2022-11-28 21:28:32 -0500

    added freezes for constants as per rubocop

--- game.rb
+++ game.rb
@@ -3,9 +3,9 @@ require_relative 'board'
 require_relative 'player'
 
 game = Board.new
-SYMBOL_CHOICES = %w[X O 1 2]
-PLAYCHOICES = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-WINNING_COMBOS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+SYMBOL_CHOICES = %w[X O 1 2].freeze
+PLAYCHOICES = [1, 2, 3, 4, 5, 6, 7, 8, 9].freeze
+WINNING_COMBOS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]].freeze
 
 puts "Welcome to Tic-Tac-Toe, Ruby Version! \nPlease select the symbol the first player would like to use for the game."
 puts "\n    1. X\n    2. O\n"
